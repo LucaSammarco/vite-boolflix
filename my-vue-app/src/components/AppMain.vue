5e54ff2fe8282bd19690e26a70bc9bd27997f2e6
Il tuo nome <la_tua_email@example.com>
2024-06-14 17:11:18 +0200

    imported api

--- my-vue-app/src/components/AppMain.vue
+++ my-vue-app/src/components/AppMain.vue
@@ -5,9 +5,44 @@ import { store } from '../store.js'
 
 export default {
     data() {
-        return {};
-    }
+        return {
+            store
+        };
+    },
+
+    methods: {
+        getMovie(){
+
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=3fbc7aaa434f8e78f2cdba0457ea9346&query=ritorno+al+futuro')
+    .then((response) =>  {
+        // handle success
+        console.log(response.data);
+        this.store.movie = response.data;
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
+
+}
+    },
+    created(){
+            this.getMovie();
+            
+        }
+
+
+
 };
+
+
+
+
+
+
 </script>
 
 <template>
