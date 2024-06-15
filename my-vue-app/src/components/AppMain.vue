640393dbc9fd65293f67c204885a831c229fcbb9
Il tuo nome <la_tua_email@example.com>
2024-06-15 16:54:23 +0200

    Added flags to searched movies

--- my-vue-app/src/components/AppMain.vue
+++ my-vue-app/src/components/AppMain.vue
@@ -38,6 +38,7 @@ axios.get('https://api.themoviedb.org/3/search/movie?api_key=3fbc7aaa434f8e78f2c
     // handle success
     console.log(response.data);
     this.store.searchResult = response.data;
+    this.store.search = ''
 })
 .catch(function (error) {
     // handle error
@@ -71,16 +72,24 @@ axios.get('https://api.themoviedb.org/3/search/movie?api_key=3fbc7aaa434f8e78f2c
 </script>
 
 <template>
-    <div>
-        <h1 class="">Main</h1>
-
-
-    </div>
-
+   
+   <main>
     <div class="input-group mb-3 p-5">
             <input type="text" v-model="store.search" class="form-control" placeholder="Movie Title..." aria-label="Recipient's username" aria-describedby="button-addon2">
             <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="searchMovie()">Search Movie</button>
     </div>
+
+
+    <div class="movies d-flex flex-wrap">
+            <article v-for="movie in store.searchResult.results" :key="movie.id" class="movie-item">
+                        <h2>Titolo: {{ movie.title }}</h2>
+                        <p>Titolo Originale: {{ movie.original_title }}</p>
+                        <p>Lingua:  <img :src="'https://flagsapi.com/' + (movie.original_language === 'en' ? 'US' : movie.original_language.toUpperCase()) + '/flat/16.png'" alt="Flag"></p>
+                        <p>Voto: {{ movie.vote_average }}</p>
+            </article>
+</div>
+
+    </main>
 
 </template>
 
@@ -88,6 +97,15 @@ axios.get('https://api.themoviedb.org/3/search/movie?api_key=3fbc7aaa434f8e78f2c
 
 @use '../styles/partial/variables' as *;
 
+main {
+    padding: 2rem;
+    margin: 1rem;
+}
 
+.movie {
+
+    padding: 2rem;
+
+}
 
 </style>
